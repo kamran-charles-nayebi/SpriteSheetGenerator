1900662f76df95bcabc6d7f2d70ea5e67ad7693f
Kamran Charles Nayebi <2135433@cegeplimoilou.ca>
2024-01-26 16:21:23 -0500

    Changed 4 specific methods for EmptySpaceTrimmerTask into one generic method

--- src/main/java/EmptySpaceTrimmerService.java
+++ src/main/java/EmptySpaceTrimmerService.java
@@ -20,13 +20,18 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.Math.abs;
 
 public class EmptySpaceTrimmerService extends Service<Void> {
 
     private List<File> files;
 
     private File savePath;
+
+    enum SIDE{TOP,RIGHT,LEFT,BOTTOM}
 
     public EmptySpaceTrimmerService(List<File> files, File savePath) {
         this.files = files;
@@ -47,10 +52,10 @@ public class EmptySpaceTrimmerService extends Service<Void> {
             for (int i = 0; i < files.size(); i++) {
                 updateProgress(i+1, files.size());
                 BufferedImage sprite = ImageIO.read(files.get(i));
-                int rowsOfEmptyPixelsLeft = findRowsOfEmptyPixelsLeft(sprite);
-                int rowsOfEmptyPixelsTop = findRowsOfEmptyPixelsTop(sprite);
-                int rowsOfEmptyPixelsBottom = findRowsOfEmptyPixelsBottom(sprite);
-                int rowsOfEmptyPixelsRight = findRowsOfEmptyPixelsRight(sprite);
+                int rowsOfEmptyPixelsLeft = findRowsOfEmptyPixels(sprite, SIDE.LEFT);
+                int rowsOfEmptyPixelsTop = findRowsOfEmptyPixels(sprite, SIDE.TOP);
+                int rowsOfEmptyPixelsBottom = findRowsOfEmptyPixels(sprite, SIDE.BOTTOM);
+                int rowsOfEmptyPixelsRight = findRowsOfEmptyPixels(sprite, SIDE.RIGHT);
                 BufferedImage resizedImage = new BufferedImage(sprite.getWidth() - rowsOfEmptyPixelsRight - rowsOfEmptyPixelsLeft,
                         sprite.getHeight() - rowsOfEmptyPixelsBottom - rowsOfEmptyPixelsTop, BufferedImage.TYPE_INT_ARGB);
                 for (int x = 0; x < resizedImage.getWidth(); x++) {
@@ -62,46 +67,26 @@ public class EmptySpaceTrimmerService extends Service<Void> {
             }
             return null;
         }
+        static int findRowsOfEmptyPixels(BufferedImage image, SIDE side) {
+            int rowsOfEmptyPixels = 0, direction, start, wrap;
+            boolean rotated = false;
+            switch (side){
+                case TOP: start = 0; direction = 1; wrap = image.getWidth(); break;
+                case BOTTOM: start = image.getHeight() - 1; direction = -1; wrap = image.getWidth(); break;
+                case LEFT: start = 0; direction = 1; wrap = image.getHeight(); rotated = true; break;
+                case RIGHT: start = image.getWidth() - 1; direction = -1; wrap = image.getWidth(); rotated = true; break;
+                default: throw new RuntimeException("No side provided");
+            }
+            int stop = start == 0 ? rotated ? image.getWidth(): image.getHeight() : 0;
 
-        int findRowsOfEmptyPixelsTop(BufferedImage image) {
-            int rowsOfEmptyPixelsTop = 0;
-            for (int y = 0; y < image.getHeight() && rowsOfEmptyPixelsTop == 0; y++) {
-                for (int x = 0; x < image.getWidth(); x++) {
-                    if ((image.getRGB(x, y) & 0xff000000) >>> 24 != 0)
-                        rowsOfEmptyPixelsTop = y-1;
+            for (int x = start; x != stop && rowsOfEmptyPixels == 0; x += direction) {
+                for (int y = 0; y < wrap; y++) {
+                    if ((image.getRGB(rotated ? x : y, rotated ? y : x) & 0xff000000) >>> 24 != 0){
+                        rowsOfEmptyPixels = abs(x-start);
+                    }
                 }
             }
-            return rowsOfEmptyPixelsTop;
-        }
-        int findRowsOfEmptyPixelsLeft(BufferedImage image) {
-            int rowsOfEmptyPixelsLeft = 0;
-            for (int x = 0; x < image.getWidth() && rowsOfEmptyPixelsLeft == 0; x++) {
-                for (int y = 0; y < image.getHeight(); y++) {
-                    if ((image.getRGB(x, y) & 0xff000000) >>> 24 != 0)
-                        rowsOfEmptyPixelsLeft = x-1;
-                }
-            }
-            return rowsOfEmptyPixelsLeft;
-        }
-        int findRowsOfEmptyPixelsBottom(BufferedImage image) {
-            int rowsOfEmptyPixelsBottom = 0;
-            for (int y = image.getHeight()-1; y != 0 && rowsOfEmptyPixelsBottom == 0; y--) {
-                for (int x = 0; x < image.getWidth(); x++) {
-                    if ((image.getRGB(x, y) & 0xff000000) >>> 24 != 0)
-                        rowsOfEmptyPixelsBottom = image.getHeight()-y-1;
-                }
-            }
-            return rowsOfEmptyPixelsBottom;
-        }
-        int findRowsOfEmptyPixelsRight(BufferedImage image) {
-            int rowsOfEmptyPixelsRight = 0;
-            for (int x = image.getWidth()-1; x != 0 && rowsOfEmptyPixelsRight == 0; x--) {
-                for (int y = 0; y < image.getHeight(); y++) {
-                    if ((image.getRGB(x, y) & 0xff000000) >>> 24 != 0)
-                        rowsOfEmptyPixelsRight = image.getWidth()-x-1;
-                }
-            }
-            return rowsOfEmptyPixelsRight;
+            return rowsOfEmptyPixels;
         }
     }
 }
